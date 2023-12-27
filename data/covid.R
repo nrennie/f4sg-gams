58ed98fd64cec8dec32846c1e7c662b0e1f8587f
nrennie <nrennie35@gmail.com>
2023-12-27 19:20:29 +0000

    update covid data

--- data/covid.R
+++ data/covid.R
@@ -5,7 +5,7 @@ covid_data <- COVID19::covid19(c("GBR", "FRA"))
 
 # Data from 10 Jan 2021 to 19 May 2022 since this is complete data
 # Documentation: https://covid19datahub.io/articles/docs.html
-covid <- covid_data |> 
+covid_raw <- covid_data |> 
   tibble::as_tibble() |> 
   dplyr::select(iso_alpha_3, date, deaths, confirmed, tests,
                 vaccines, people_vaccinated, people_fully_vaccinated,
@@ -14,4 +14,22 @@ covid <- covid_data |>
   dplyr::filter(date <= lubridate::ymd("20220519")) 
 
 # save to CSV
+readr::write_csv(covid_raw, "data/covid_raw.csv")
+
+# rescale per 100,000 population
+covid <- covid_raw |> 
+  dplyr::mutate(
+    dplyr::across(deaths:hosp, ~ (100000 * .x) / population)
+  ) |> 
+  dplyr::select(-population)
+
+# save to CSV
 readr::write_csv(covid, "data/covid.csv")
+
+
+
+
+
+
+
+
