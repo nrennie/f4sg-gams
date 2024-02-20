67eb8918c6d19b74e559a6eca47ed219bd38eeb6
nrennie <nrennie35@gmail.com>
2024-02-20 13:07:36 +0000

    add example 4

--- examples/example_04.R
+++ examples/example_04.R
@@ -1,3 +1,13 @@
-# when it goes wrong
+# look at autocorrelation in residuals
+# caution: in this example, data is not equally spaced!
+acf(gam_6$residuals)
+pacf(gam_6$residuals)
 
-gamm
+# fit a `gamm()`
+gamm_1 <- gamm(confirmed ~ s(date_obs) + s(day, k = 7), data = gbr_train)
+
+# add a correlation structure
+gamm_2 <- gamm(confirmed ~ s(date_obs) + s(day, k = 7),
+  correlation = corARMA(form = ~ 1 | date_obs, p = 1),
+  data = gbr_train
+)
---
+++ examples_blank/example_04.R
@@ -0,0 +1,8 @@
+# look at autocorrelation in residuals
+# caution: in this example, data is not equally spaced!
+
+
+# fit a `gamm()`
+
+# add a correlation structure
+
