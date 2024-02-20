5af7ebd5af8398942cc1ea0de93f1d96c934c870
nrennie <nrennie35@gmail.com>
2024-02-20 01:42:55 +0000

    update example 1

--- examples/example_01.R
+++ examples/example_01.R
@@ -15,12 +15,15 @@ gbr_train <- gbr_data[1:ceiling(train_prop * nrow(gbr_data)),]
 gbr_test <- gbr_data[(ceiling(train_prop * nrow(gbr_data)) + 1):nrow(gbr_data),]
 
 # Plot the data
-plot(gbr_train$date, gbr_train$deaths,
+plot(gbr_train$date, gbr_train$confirmed,
      type = "l",
-     xlab = "Date", ylab = "Cumulative deaths per 100,000")
+     xlab = "Date", ylab = "Confirmed cases per day")
 
 # Which other variables may be of interest?
+plot(gbr_train$tests, gbr_train$confirmed,
+     xlab = "Tests performed", ylab = "Cumulative cases per 100,000")
 
-
-
+# fit a linear model using gam()
+lm_gam <- gam(confirmed ~ date_obs, data = gbr_train)
+coef(lm_gam)
 
