880c737274833b113afdcc6ba597e69065b7483b
nrennie <nrennie35@gmail.com>
2024-02-20 13:16:21 +0000

    add example 3

---
+++ examples_blank/example_03.R
@@ -0,0 +1,15 @@
+# Model check
+
+
+# k = 10 is the default
+# it seems too small
+
+
+# Obtain fitted values
+# Same as `glm` function in R
+
+
+# Predictions for new data
+
+
+# Model interpretation
