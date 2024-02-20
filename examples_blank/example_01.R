150d1988cc86d0c1cd8abf7ce6ef6ca8595f47d5
nrennie <nrennie35@gmail.com>
2024-02-20 01:43:46 +0000

    add blank example 1

---
+++ examples_blank/example_01.R
@@ -0,0 +1,20 @@
+# Load packages
+
+
+# Load data
+
+
+# Subset data to look at just GBR
+
+
+# Split data to train and test sets
+
+
+# Plot the data
+
+
+# Which other variables may be of interest?
+
+
+# fit a linear model using gam()
+
