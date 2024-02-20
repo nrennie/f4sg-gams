b05490aed53a94b16601ac96f95a47fcd703d550
nrennie <nrennie35@gmail.com>
2024-02-20 13:07:51 +0000

    tidy up example 2

--- examples/example_02.R
+++ examples/example_02.R
@@ -1,6 +1,7 @@
 # fit model using gam()
 gam_1 <- gam(confirmed ~ s(date_obs), data = gbr_train)
 coef(gam_1)
+summary(gam_1)
 
 # Plot the linear predictor
 plot(gam_1)
@@ -11,10 +12,13 @@ lines(gbr_train$date_obs, gam_1$fitted.values, col = "red")
 
 # Adding additional terms
 gam_2 <- gam(confirmed ~ s(date_obs) + s(tests), data = gbr_train)
-coef(gam_2)
-plot(gam_2)
+summary(gam_2)
 
-gam_3 <- gam(confirmed ~ s(date_obs) + s(day, k = 7), data = gbr_train, method = "REML")
+gam_3 <- gam(confirmed ~ s(date_obs) + s(tests) + s(day, k = 7), 
+             data = gbr_train)
+summary(gam_3)
+
+# plot fitted values
 plot(gbr_train$date_obs, gbr_train$confirmed,
      type = "l",
      xlab = "Day of observation", ylab = "Confirmed cases per day")
@@ -26,8 +30,10 @@ gam_4 <- gam(confirmed ~ s(date_obs, sp = 0.4), data = gbr_train)
 plot(gam_4)
 
 # "REML"
-gam_5 <- gam(confirmed ~ s(date_obs) + s(day, k = 7), data = gbr_train, method = "REML")
-plot(gam_5)
+gam_5 <- gam(confirmed ~ s(date_obs) + s(tests) + s(day, k = 7),
+             data = gbr_train,
+             method = "REML")
+summary(gam_5)
 
 # Visualising
 library(gratia)
