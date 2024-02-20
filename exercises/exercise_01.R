fb09b09d9d4bda917b5d0cdbafb32e9bb460c3ea
nrennie <nrennie35@gmail.com>
2024-02-20 02:14:28 +0000

    add exercise 1

---
+++ exercises/exercise_01.R
@@ -0,0 +1,22 @@
+# Load packages
+library(tidyverse)
+library(mgcv)
+
+# Load data
+covid <- read_csv("data/covid.csv")
+
+# Subset data to look at just FRA
+
+
+# Split data to train and test sets
+
+
+# Plot the data
+
+
+# Which other variables may be of interest?
+
+
+# fit a linear model using gam()
+
+
